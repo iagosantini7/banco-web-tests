0bb2b16be03755d8be3e0b4fafd4d532a3a16e50
Iago Santini <iagosantini@gmail.com>
2026-06-07 22:21:04 -0300

    Adicionando o cy.screeshot removido

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -3,6 +3,7 @@ describe('Login', () => {
   beforeEach(() => {
     //Arrange
     cy.visit('http://localhost:4000/')
+    cy.screenshot('apos-visitar-pagina')
   })
   
   it('Login com dados validos deve permitir entrada no sistema', () => {
@@ -11,6 +12,7 @@ describe('Login', () => {
       .click()
       .type('julio.lima')
     cy.get('#senha').click().type(123456)
+    cy.screenshot('apos-preencher-com-dados-validos')
     cy.contains('button', 'Entrar').click()
     //Assert 
     cy.contains('h4', 'Realizar Transferência').should('be.visible')
